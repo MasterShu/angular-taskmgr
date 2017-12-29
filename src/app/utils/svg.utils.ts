dcb3f59d25c8fc3a3121198048be6acc4e9c2442
MasterShu <maste2shu@foxmail.com>
2017-12-29 09:46:38 +0800

    add material & init project

---
+++ src/app/utils/svg.utils.ts
@@ -0,0 +1,13 @@
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+
+export const loadSvgResources = (ir: MatIconRegistry, ds: DomSanitizer)  => {
+  const imgDir = 'assets/img';
+  const sidebarDir = `${imgDir}/sidebar`;
+  ir.addSvgIcon('day', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/day.svg`));
+  ir.addSvgIcon('month', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/month.svg`));
+  ir.addSvgIcon('project', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/project.svg`));
+  ir.addSvgIcon('projects', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/projects.svg`));
+  ir.addSvgIcon('week', ds.bypassSecurityTrustResourceUrl(`${sidebarDir}/week.svg`));
+};
+
